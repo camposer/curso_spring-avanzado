17788bedc55aa4392ef87cd96d5b6c9bd79ce5de
Rodolfo Campos <camposer@gmail.com>
2014-10-21 17:43:52 +0200

    Borrado ejercicio1.old (backup - versión anterior)

--- ejercicio1.old/src/main/java/springavanzado/ejercicio1/dao/OrdenadorDao.java
+++
@@ -1,7 +0,0 @@
-package springavanzado.ejercicio1.dao;
-
-import springavanzado.model.Ordenador;
-
-public interface OrdenadorDao extends GenericDao<Ordenador, Integer> {
-	
-}
--- ejercicio1.old/src/main/java/springavanzado/ejercicio1/dao/PersonaDao.java
+++
@@ -1,8 +0,0 @@
-package springavanzado.ejercicio1.dao;
-
-import springavanzado.model.Persona;
-
-public interface PersonaDao 
-	extends GenericDao<Persona, Integer> {
-	
-}
